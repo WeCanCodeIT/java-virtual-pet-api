5abc558db4083c30dbadb9609c1f58861dea7d70
Columbus Instructor <instructor@wecancodeit.org>
2018-11-13 11:46:58 -0500

    Update API to return all pets

--- src/main/java/org/wecancodeit/javavirtualpetapi/controllers/ApiController.java
+++ src/main/java/org/wecancodeit/javavirtualpetapi/controllers/ApiController.java
@@ -1,8 +1,10 @@
 package org.wecancodeit.javavirtualpetapi.controllers;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.wecancodeit.javavirtualpetapi.models.Pet;
 import org.wecancodeit.javavirtualpetapi.repositories.PetRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class ApiController {
@@ -20,6 +23,11 @@ public class ApiController {
 	@GetMapping({"", "/", "/index"})
 	public String foo() {
 		return "Hello API";
+	}
+	
+	@GetMapping("/pets")
+	public Collection<Pet> getPets() {
+		return (Collection<Pet>) repo.findAll();
 	}
 	
 	@GetMapping("/pets/{id}")
